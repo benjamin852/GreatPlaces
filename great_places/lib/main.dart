d1eb93ca375a611c44e0c08414b9796b1ceec7ab
Ben Weinberg <weinbergben33@gmail.com>
2020-01-31 13:59:05 -0500

    wired up provider in main

--- great_places/lib/main.dart
+++ great_places/lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:great_places/provider/great_places.dart';
+
+import 'package:great_places/screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.amber,
+        ),
+        home: PlaceListScreen(),
       ),
-      home: 
     );
   }
 }
