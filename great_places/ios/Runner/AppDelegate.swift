3522d730d4d50264f37998f67ac47350969c19ce
Ben Weinberg <weinbergben33@gmail.com>
2020-02-04 12:43:45 -0500

    working map

--- great_places/ios/Runner/AppDelegate.swift
+++ great_places/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBAuyWXoUUD6FwKN-QZJK4AcCD4JnGZCe0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- great_places/lib/widgets/location_input.dart
+++ great_places/lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:great_places/helpers/location_helper.dart';
+import 'package:great_places/screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -12,8 +13,24 @@ class _LocationInputState extends State<LocationInput> {
   Future<void> _getCurrentUserLocation() async {
     final locationData = await Location().getLocation();
     final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
-        locationData.latitude, locationData.longitude);
+      locationData.latitude,
+      locationData.longitude,
+    );
     setState(() => _previewImageUrl = staticMapImageUrl);
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
   }
 
   @override
@@ -54,7 +71,7 @@ class _LocationInputState extends State<LocationInput> {
               icon: Icon(Icons.map),
               label: Text('Select On Map'),
               textColor: Theme.of(context).primaryColor,
-              onPressed: () {},
+              onPressed: _selectOnMap,
             ),
           ],
         )
@@ -62,3 +79,93 @@ class _LocationInputState extends State<LocationInput> {
     );
   }
 }
+/*
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      locData.latitude,
+      locData.longitude,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    // ...
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text(
+                  'No Location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(
+                Icons.location_on,
+              ),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: _getCurrentUserLocation,
+            ),
+            FlatButton.icon(
+              icon: Icon(
+                Icons.map,
+              ),
+              label: Text('Select on Map'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: _selectOnMap,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+*/
